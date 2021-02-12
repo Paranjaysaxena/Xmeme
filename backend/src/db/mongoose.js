a510c447933d22fe51e8aade510997c6755d0fce
Paranjaysaxena <42527319+Paranjaysaxena@users.noreply.github.com>
2021-02-12 16:02:39 +0530

    add docker

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/meme-api', {
+mongoose.connect('mongodb://mongo:27017/meme-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
